7c8db0e58bf9109628e8e6f00b7eb51dc0177e67
Criss <wtopull@gmail>
2020-03-09 14:39:31 +0800

    设计师页面完成

---
+++ src/components/cards/designer-card.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="designer_card flex j_sb">
+    <div class="card_left flex">
+      <div class="card_left__img">
+        <img src="@/assets/images/along.jpg" alt />
+      </div>
+      <div class="card_left_info flex flex_column">
+        <p class="info_username">阿尨</p>
+        <p class="info_type">深圳 | 插画师</p>
+        <p class="info_works">
+          创作
+          <span>93</span> |
+          <span>12912</span>粉丝
+        </p>
+        <p class="info_signature">公众号：a_long_72 淘宝店：滚货铺</p>
+        <!-- <button class="focus_n">关注</button> -->
+        <button class="focus_y">已关注</button>
+      </div>
+    </div>
+    <div class="card_right flex_items">
+      <ul class="flex_items">
+        <li>
+          <!-- <img :src="require(`@/assets/images/${advLongUrl.img}.jpg`)" alt=""> -->
+          <img src="@/assets/images/along_pro.jpg" alt />
+        </li>
+        <li>
+          <img src="@/assets/images/along_pro2.jpg" alt />
+        </li>
+        <li>
+          <img src="@/assets/images/along_pro3.jpg" alt />
+        </li>
+        <li>
+          <img src="@/assets/images/along_pro4.jpg" alt />
+        </li>
+      </ul>
+      <div class="seize_box flex_a_j">
+        <a-icon type="ellipsis" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  components: {}
+};
+</script>
+<style lang="scss" scoped>
+.card_left {
+  & .card_left__img {
+    img {
+      width: 80px;
+      height: 80px;
+      border: none;
+      border-radius: 50%;
+      overflow: hidden;
+    }
+  }
+  & .card_left_info {
+    margin-left: 20px;
+    & .info_username {
+      margin-bottom: 2px;
+      color: #333;
+      font-size: 16px;
+      font-weight: 600;
+      cursor: pointer;
+    }
+    & .info_username:hover {
+      color: #d36f16;
+    }
+    & .info_type {
+      margin-bottom: 12px;
+      font-size: 12px;
+      color: #bbb;
+    }
+    & .info_works {
+      font-size: 14px;
+      color: #666;
+      margin-bottom: 0px;
+      & span {
+        font-weight: 600;
+        color: #333;
+        cursor: pointer;
+      }
+      & span:hover {
+        color: #d36f16;
+      }
+    }
+    & .info_signature {
+      margin-top: 2px;
+      display: inline-block;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      font-size: 12px;
+      color: #bbb;
+      width: 220px;
+    }
+    & button {
+      margin-top: 15px;
+      width: 100px;
+      height: 36px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    & button.focus_n {
+      color: #444;
+      background: #ffe300;
+      border: 1px solid #ffe300;
+    }
+    & button.focus_n:hover {
+      color: #444;
+      background: #ffd100;
+      border: 1px solid #ffd100;
+    }
+    & button.focus_y {
+      color: #666;
+      background: 0 0;
+      border: 1px solid #bbb;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    & button.focus_y:hover {
+      color: #444;
+      background: 0 0;
+      border: 1px solid #999;
+    }
+  }
+}
+.card_right {
+  & li {
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  & li:last-child {
+    margin-right: 0;
+  }
+  & img {
+    width: 200px;
+    height: 160px;
+  }
+  & img:hover {
+    opacity: 0.8;
+  }
+}
+.seize_box {
+  width: 26px;
+  height: 160px;
+  margin-left: 20px;
+  margin-right: -10px;
+  cursor: pointer;
+  & i {
+    font-size: 44px;
+    font-weight: 700;
+    color: #ffd100;
+    transform: rotate(90deg);
+  }
+  & i:hover {
+    color: #444;
+  }
+}
+</style>
--- src/views/designer/index.vue
+++ src/views/designer/index.vue
@@ -3,12 +3,20 @@
     <div class="designer_t">
       <f-sub-nav :subNav="subNav" :targetY="56" @subNavPath="subNavPath"></f-sub-nav>
     </div>
-    <f-paging :pageNum="workLists.length" :pageIndex="pageIndex" @setPage="setPage"></f-paging>
+    <div class="designer_c mauto">
+      <ul>
+        <li v-for="(item,index) in 10" :key="index">
+          <f-designer-card></f-designer-card>
+        </li>
+      </ul>
+    </div>
+    <f-paging :pageNum="10" :pageIndex="pageIndex" @setPage="setPage"></f-paging>
   </div>
 </template>
 <script>
 import fSubNav from "@/components/subNav/index";
 import fPaging from "@/components/paging/index";
+import fDesignerCard from "@/components/cards/designer-card";
 import workLists from "@/assets/js/workLists";
 import scroll from "@/assets/js/scrollToTop";
 export default {
@@ -17,8 +25,8 @@ export default {
       pageIndex: 40,
       workLists: workLists,
       subNav: [
-        { title: "编辑精选", path: "/" },
-        { title: "最新发布", path: "/discover" }
+        { title: "设计师", path: "/" },
+        { title: "团体", path: "/discover" }
       ]
     };
   },
@@ -29,7 +37,7 @@ export default {
       const currentY =
         document.documentElement.scrollTop || document.body.scrollTop;
       // 调用滚动方法
-      scroll(currentY, 680);
+      scroll(currentY, 56);
       this.lists = workLists.slice(
         (n - 1) * this.pageIndex,
         this.pageIndex * n
@@ -45,12 +53,24 @@ export default {
   },
   components: {
     fSubNav,
-    fPaging
+    fPaging,
+    fDesignerCard
   }
 };
 </script>
 <style lang="scss" scoped>
 .designer_t {
   background: #ffffff;
+}
+.designer_c {
+  margin-top: 20px;
+  border-radius: 4px;
+  overflow: hidden;
+  & li {
+    padding: 30px;
+    overflow: hidden;
+    background: #fff;
+    border-bottom: 1px solid #eee;
+  }
 }
 </style>
